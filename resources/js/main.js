355ff13aaf8a3b671aa7699f9edabc0ba39662ea
Lindsay Rosillo <LindsayRosillo@lindsaysmacbook.attlocal.net>
2018-05-04 12:28:40 -0400

    Add final styling and bottom help section

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $('.carousel').slick({
-    autoplay: true
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
   });
-});
+  });
